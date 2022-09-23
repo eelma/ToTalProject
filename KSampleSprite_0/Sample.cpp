38b84aed238b959d697e721d606afcb7943e78d3
구 <kgcaA@kgcaschool.com>
2022-09-23 18:48:08 +0900

    11

--- KSampleSprite_0/Sample.cpp
+++ KSampleSprite_0/Sample.cpp
@@ -1,126 +1,10 @@
 #include "Sample.h"
-#include"KTextureManager.h"
-void KObject2D::UpdateVertexBuffer()
-{
-    m_VertexList[0].p = { m_vDrawPos.x, m_vDrawPos.y,  0.0f };
-    m_VertexList[0].t = { m_rtUV.x1,m_rtUV.y1 };
-
-    m_VertexList[1].p = { m_vDrawPos.x+m_vDrawSize.x, m_vDrawPos.y,  0.0f };
-    m_VertexList[1].t = { m_rtUV.x1+m_rtUV.w,m_rtUV.y1 };
-
-    m_VertexList[2].p = { m_vDrawPos.x, m_vDrawPos.y-m_vDrawSize.y, 0.0f };
-    m_VertexList[2].t = { m_rtUV.x1,m_rtUV.y1+m_rtUV.h };
-
-    m_VertexList[3].p = m_VertexList[2].p;
-    m_VertexList[3].t = m_VertexList[2].t;
-
-    m_VertexList[4].p = m_VertexList[1].p;
-    m_VertexList[4].t = m_VertexList[1].t;
-
-    m_VertexList[5].p = { m_vDrawPos.x+m_vDrawSize.x, m_vDrawPos.y - m_vDrawSize.y, 0.0f };
-    m_VertexList[5].t = { m_rtUV.x1+m_rtUV.w,m_rtUV.y1 + m_rtUV.h };
-
-    m_pImmediateContext->UpdateSubresource(
-        m_pVertexBuffer, NULL, NULL,&m_VertexList.at(0), 0, 0);
-}
-void KObject2D::SetRect(KRect rt)
-{
-    m_rtInit = rt;
-    m_pkImageSize.x = m_pTexture->m_Desc.Width;
-    m_pkImageSize.y = m_pTexture->m_Desc.Height;
-    //image w,h
-    //사실상 정점에 들어가는 uv값
-    m_rtUV.x1 = rt.x1 / m_pkImageSize.x;//u
-    //1
-    m_rtUV.y1 = rt.y1 / m_pkImageSize.y;//v
-    //40
-    m_rtUV.w = rt.w / m_pkImageSize.x;
-    //60
-    m_rtUV.h = rt.h/ m_pkImageSize.y;
-    
-}
-//vPos(화면 좌표)를 ndc 좌표로 변경(컨버팅)해야한다
-//화면 좌표로 세팅하고 내부적으로 ndc로 랜더링을한다
-//ndc좌표를 만들어주는 기능이 필요하다
-void KObject2D::SetPosition(KVector2D vPos)
-{
-    m_vPos = vPos;
-    //0~800 -> 0~1-> -1~+1
-    //ndc좌표
-    m_vDrawPos.x = (vPos.x / g_rtClient.right)*2.0f-1.0f;
-    m_vDrawPos.y = -((vPos.y / g_rtClient.bottom) * 2.0f - 1.0f);
-    m_vDrawSize.x = (m_rtInit.w / g_rtClient.right)*2;
-    m_vDrawSize.y = ((m_rtInit.h / g_rtClient.bottom))*2;
-    UpdateVertexBuffer();
-}
-bool KObject2D::Frame()
-{
-
-    return true;
-}
 
 
-bool KNpc2D::Frame()
-{
-    KVector2D vPos = m_vPos;
-    //벡터의 직선의 방정식 &  시간의 동기화
-    KVector2D  m_vVelocity = m_vDir * m_fSpeed * g_fSecondPerFrame;
-    vPos = vPos + m_vVelocity;
-   /* m_fSpeed -= 1.0f*g_fSecondPerFrame;
-    if (0 >= m_fSpeed)
-    {
-        m_fSpeed = rand() % 10;
-    }*/
 
-    if (vPos.x > g_rtClient.right)
-    {
-        vPos.x = g_rtClient.right;
-        m_vDir.x *= -1.0f;
-    }
-    if (vPos.x < 0.0f)
-    {
-        vPos.x = 0.0f;
-        m_vDir.x *= -1.0f;
-    }
-    if (vPos.y > g_rtClient.bottom)
-    {
-        vPos.y = g_rtClient.bottom;
-        m_vDir.y *= -1.0f;
-    }
-    if (vPos.y < 0.0f)
-    {
-        vPos.y = 0.0f;
-        m_vDir.y *= -1.0f;
-    }
-    
-    SetPosition(vPos);
-    return true;
-}
 
-bool KUser2D::Frame()
-{
-    KVector2D vPos = m_vPos;
-    if (I_Input.GetKey('W'))
-    {
-        vPos.y +=-1.0f* g_fSecondPerFrame * m_fSpeed;
-    }
-    if (I_Input.GetKey('S'))
-    {
-        vPos.y += 1.0f * g_fSecondPerFrame * m_fSpeed;
-    }
-    if (I_Input.GetKey('A'))
-    {
-        vPos.x += -1.0f * g_fSecondPerFrame * m_fSpeed;
-    }
-    if (I_Input.GetKey('D'))
-    {
-        vPos.x += 1.0f * g_fSecondPerFrame * m_fSpeed;
-    }
-    SetPosition(vPos);
-    return true;
-}
 
-bool	Sample::Init()
+bool Sample::Init()
 {
     KTexture*pMaskTex= I_Tex.Load(L"../../data/bitmap2.bmp");
    
@@ -167,7 +51,7 @@ bool	Sample::Init()
     
     return true;
 }
-bool		Sample::Frame()
+bool Sample::Frame()
 {
     m_pMap->Frame();
     for (auto obj : m_pNpcList)
@@ -177,7 +61,7 @@ bool		Sample::Frame()
     m_pUser->Frame();
     return true;
 }
-bool		Sample::Render()
+bool Sample::Render()
 {
     m_pMap->Render();
     for (auto obj : m_pNpcList)
@@ -191,7 +75,7 @@ bool		Sample::Render()
     m_pUser->PostRender();
     return true;
 }
-bool		Sample::Release()
+bool Sample::Release()
 {
     m_pMap->Release();
     m_pUser->Release();
--- KSampleSprite_0/Sample.h
+++ KSampleSprite_0/Sample.h
@@ -1,51 +1,8 @@
 #pragma once
 #include "KGameCore.h"
-#include"KBaseObject.h"
-class KObject2D :public  KBaseObject
-{
-public:
-	KTexture* m_pMaskTex = nullptr;
-	POINT m_pkImageSize;
-	RECT m_rt;//이미지에 대한 크기
-	KRect m_rtInit;
-	KRect m_rtUV;
-	//스크린의 포지션
-	KVector2D m_vPos;
-	//	KVector2D m_vSize;
-	KVector2D m_vDir;
-	float m_fSpeed = 100.0f;
-private:
-	KVector2D m_vDrawPos;
-	KVector2D m_vDrawSize;
+#include"KUser2D.h"
+#include"KNpc2D.h"
 
-public:
-	bool Frame()override;
-	void SetPosition(KVector2D vPos);
-	void SetDirection(KVector2D vDir)
-	{
-		m_vDir = vDir;
-	}
-	void SetRect(KRect rt);
-	void UpdateVertexBuffer();
-	void SetMask(KTexture* pMaskTex)
-	{
-		m_pMaskTex = pMaskTex;
-	}
-
-};
-class KUser2D :public KObject2D
-{
-
-public:
-	bool Frame()override;
-};
-class KNpc2D :public KObject2D
-{
-
-public:
-	bool Frame()override;
-	
-};
 class Sample : public KGameCore
 {
 	KBaseObject*			m_pMap;
