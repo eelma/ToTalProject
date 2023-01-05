5c6fb051319d3915338401906f0e01f1bd908a22
구 <kgcaA@kgcaschool.com>
2023-01-05 20:19:16 +0900

    11

---
+++ Socket/Socket.cpp
@@ -0,0 +1,110 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include<WinSock2.h>
+#include<iostream>
+//#define SERVER_CODE
+#ifndef SERVER_CODE
+
+
+int main()
+{
+
+	WSADATA wsa;
+	if(WSAStartup(MAKEWORD(2,2), &wsa) != 0)
+	{
+		return 0;
+	}
+	SOCKET sock3 = socket(AF_INET, SOCK_STREAM, 0);
+	//client
+
+	SOCKADDR_IN sa;//목적지+포트
+
+	sa.sin_family = AF_INET;
+
+	sa.sin_addr.s_addr = inet_addr("192.168.0.86");
+
+	sa.sin_port = htons(10000);
+	int iRet = connect(sock3, (sockaddr*)&sa, sizeof(sa));
+	if (iRet == SOCKET_ERROR) { return 1; }
+	char szSendMsg[256] = { 0, };
+	fgets(szSendMsg, 256, stdin);
+	int iSendBytes = send(sock3, szSendMsg, strlen(szSendMsg), 0);
+	char szRecvMsg[256] = { 0, };
+	int iRecvBytes = recv(sock3, szSendMsg, 256, 0);
+	printf("%s\n", szRecvMsg);
+	closesocket(sock3);
+	WSACleanup();
+	std::cout << "wtf";
+}
+
+
+#else
+
+int main()
+{
+	//윈속 초기화 정보 구조체
+	WSADATA wsa;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+		//첫번째 인수는 윈속의 버전을 지정한다 makeword는 2개의 인수를 word로 pack 하는 매크로다
+		//word는 16비트의 부호가 없는 변수로 wsastartup()에서는 word의 16비트에서 상위 8비트에 major번호 하위 8비트에 minor번호를 넣어 넘겨주어야한다.
+		//wsastartup의 두번째 인수는 wsastartup이 초기화된 상태를 저장하는 변수를 넘겨준다.
+		//wsatartup()은 윈속을 초기화하고 초기화한 상태를 wsadata구조체에 저장한다
+	{
+		return 0;
+	}
+	//SOCKET sock1 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	//SOCKET sock2 = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	SOCKET sock3 = socket(AF_INET, SOCK_STREAM, 0);//TCP, 스트림 소켓,(0=인터넷 프로토콜) 주소체계, 소켓타입, 사용할 프로토콜
+	//반환값: SOCKET(32비트 정수) 성공시 새로운 소켓, 실패시 INVAILID_SOCKET
+	short sData = 10000;
+	short tData = 0x271094;
+	short fData = 0x1027;
+	
+
+	short test1 = htons(0x12345678);
+	short test2 = ntohs(0x12345678);
+	
+	//client
+	SOCKADDR_IN sa;//목적지+포트
+	sa.sin_family = AF_INET;//socket에서 주소체계를 저장하는 변수이다 TCP/IP프로토콜을 사용하면 값은 AF_INET가 된다
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);//ip주소를 나타내며 32비트 in_addr 구조체를 사용한다 INADDR_ANY는 자동으로 이 컴퓨터에 존재하는 랜카드 중 사용가능한 랜카드의 IP주소를 사용하라는 의미
+	sa.sin_port = htons(10000); //포트번호를 나타내며 부호없는 16비트 정수값을 사용한다.
+
+	//htons와 htonl은 바이트 정렬으로 네트워크에선 무조건 빅엔디안(최상위 바이트부터 차례로 저장되는 방식)을 사용한다 리틀엔디안은 거꾸로 저장해서 절대 쓰면 안된다
+	//htonl long intger(일반적으로 4byte)데이터를 네트워크 byte order로 변경한다. 호스트 바이트 -> 네트워크 바이트
+	//htons short intger(일반적으로 2byte)데이터를 네트워크 byte order로 변경한다. 호스트 바이트 -> 네트워크 바이트
+
+	int iRet = bind(sock3, (sockaddr*)&sa, sizeof(sa));//로컬 주소를 소켓과 연결한다 socket, const sockaddr*name, namelen 
+	if (iRet == SOCKET_ERROR)
+	{
+		return 1;
+	}
+	iRet = listen(sock3, SOMAXCONN);
+	if (iRet == SOCKET_ERROR)
+	{
+		return 1;
+	}
+	//접속되면 반환된다.
+	SOCKADDR_IN clientaddr;
+	int length = sizeof(clientaddr);
+	SOCKET clientSock = accept(sock3, (sockaddr*)&clientaddr, &length);//클라이언트 접속을 수용할 목적으로 만든 소켓, 소켓 주소 구조체, 소켓 주소 구조체 변수의 길이(바이트 단위)
+	//성공시 0을 반환, 실패시 -1orSOCKET_ERROR을 반환
+	printf("클라이언트 접속: IP%s, PORT:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));//inet_ntoa는 네트워크 바이트 순서의 32비트 값을 dotted decimal notation의 주소값으로 변환한다
+	// inet_addr <-> inet_ntoa
+
+	char szRecvMsg[256] = { 0, };
+	int iRecvBytes = recv(clientSock, szRecvMsg, 256, 0);
+	printf("%s\n", szRecvMsg);
+
+	int iSendBytes = send(clientSock, szRecvMsg, strlen(szRecvMsg), 0);
+
+	closesocket(sock3);
+
+	WSACleanup();
+	std::cout << "HelloWorld!\n";
+
+
+}
+
+
+#endif // !SERVER_CODE
---
+++ Socket1/Socket1.cpp
@@ -0,0 +1,101 @@
+﻿//파일디스크립터:네트워크 소켓과 같은 파일이나 기타 입력/출력 리소스에 액세스하는 데 사용되는 추상표현이다. 즉, 시스템으로 부터 할당받은 파일이나 소켓을 대표하는 정수다.
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include<WinSock2.h>
+#include<iostream>
+#define SERVER_CODE
+#ifndef SERVER_CODE
+
+
+int main()
+{
+
+	
+}
+
+
+#else
+
+int main()
+{
+	WSADATA ws;
+	if (WSAStartup(MAKEWORD(2, 2), &ws) != 0)
+	{
+		return 0;
+	}
+
+	SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	SOCKADDR_IN sa;
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = htonl(INADDR_ANY);//INADDR_ANY는 자동으로 이 컴퓨터에 존재하는 랜카드 중 사용가능한 랜카드의 IP주소를 사용하라는 의미
+	sa.sin_port = htons(10000);
+	int iRet = bind(sock, (sockaddr*)&sa, sizeof(sa));
+	if (iRet == SOCKET_ERROR)
+	{
+		return 1;
+	}
+	iRet = listen(sock, SOMAXCONN);//연결 요청 소켈이 대기하는 연결 대기열을 생성한다 윈속2부터는 somaxcoon 상수를 사용하는데 소켓 서비스 제공자가 알아서 backlog값을 설정한다.
+	if(iRet == SOCKET_ERROR)
+	{
+		return 1;
+	}
+	//접속되면 반환된다
+	SOCKADDR_IN clientaddr;
+	int length = sizeof(clientaddr);
+	SOCKET clientSock = accept(sock, (sockaddr*)&clientaddr, &length);//클라이언트 접속을 수용할 목적으로 만든 소켓, 소켓 주소 구조체, 소켓 주소 구조체 변수의 길이(바이트 단위)
+	//성공시 0을 반환, 실패시 -1orSOCKET_ERROR을 반환
+	if (clientSock == SOCKET_ERROR)
+	{
+		closesocket(sock);
+		WSACleanup();
+		return 1;
+	}
+	printf("클라이언트 접속 IP:%s, PORT: %d", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+	u_long iMode = TRUE;
+	ioctlsocket(clientSock, FIONBIO, &iMode);//논블로킹 소켓으로 만드는 소켓
+											 //논블로킹모드로 설정할 소켓, 소켓에대해 수행할 동작을 의미, cmd가 수행할 동작에 대한 판별값 0이면 블로킹모드 0이 아니면 논블로킹모드
+
+	while (1)
+	{
+		char szRecvMsg[256] = { 0, };
+		int iRecvBytes = recv(clientSock, szRecvMsg, 256, 0);//connect, accept로 연결된 소켓디스크립터, data를 수신하여 저장할 버퍼, 읽을 데이터 크기, 읽을 데이터 유형or옵션
+		if (iRecvBytes == 0)
+		{
+			printf("클라이언트 접속 종료 : IP:%s, PORT:%d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+			closesocket(clientSock);
+			break;
+		}
+		if (iRecvBytes == SOCKET_ERROR)
+		{
+			if (WSAGetLastError() != WSAEWOULDBLOCK)//WSAGetLastError은 마지막 오류를 반환
+				/*넌블록 소켓에서 WSAEWOULDBLOCK의 의미
+				WSAAcceptm, accept = 연결 요청 받지 못함
+				closesocket = SO_LINGER옵션에 타임아웃
+				WSAConnect, connect = 연결 시도
+				recv함수들 = 데이터 아직 못받음 : 다시 요청해야함
+				send 함수들 = 전송할 버퍼 공간 없음, 나중에 다시 시도
+				*/
+			{
+				//WSAEWOULDBLOCK 아니라면 오류
+				closesocket(sock);
+				return 1;
+			}
+		}
+		else
+		{
+			printf("%s \n", szRecvMsg);
+		}
+		if (iRecvBytes > 0)
+		{
+			int iSendBytes = send(clientaddr, szRecvMsg);
+		}
+
+	}
+
+
+
+}
+
+
+#endif // !SERVER_CODE
